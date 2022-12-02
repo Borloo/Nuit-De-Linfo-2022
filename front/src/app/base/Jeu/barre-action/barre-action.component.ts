97947b324d4b4ca49fc74fdff2363cc0461865a0
Borloo <supermaxime753@gmail.com>
2022-12-02 02:52:29 +0100

    Init barre action

---
+++ front/src/app/base/Jeu/barre-action/barre-action.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barre-action',
+  templateUrl: './barre-action.component.html',
+  styleUrls: ['./barre-action.component.scss']
+})
+export class BarreActionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
