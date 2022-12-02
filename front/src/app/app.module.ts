0e885a64456c84dff5a327111a66b9a562827acc
Borloo <supermaxime753@gmail.com>
2022-12-02 04:25:52 +0100

    Init application jeu

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApplicationJeuComponent } from './base/jeu/application-jeu/application-jeu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ApplicationJeuComponent,
   ],
   imports: [
     BrowserModule,
---
+++ front/src/app/base/Jeu/application-jeu/application-jeu.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ApplicationJeuRoutingModule } from './application-jeu-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ApplicationJeuRoutingModule
+  ]
+})
+export class ApplicationJeuModule { }
