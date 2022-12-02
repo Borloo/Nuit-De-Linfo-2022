0ae51d87347f0c21d29e13a08e866940db0c47dc
Baptisteuh <touchaisb@gmail.com>
2022-12-02 03:53:31 +0100

    caca

---
+++ front/src/app/base/Jeu/plateau-jeu/plateau-jeu.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlateauJeuComponent } from './plateau-jeu.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlateauJeuComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PlateauJeuComponent
+  ]
+})
+export class PlateauJeuModule { }
