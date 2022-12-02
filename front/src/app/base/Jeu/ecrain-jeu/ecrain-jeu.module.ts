ceb8b3aa477cc23625aea9ff7157f1b8b5e3b43a
Baptisteuh <touchaisb@gmail.com>
2022-12-02 03:53:58 +0100

    caca

--- front/src/app/base/Jeu/ecrain-jeu/ecrain-jeu.module.ts
+++ front/src/app/base/Jeu/ecrain-jeu/ecrain-jeu.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EcrainJeuRoutingModule } from './ecrain-jeu-routing.module';
+import { PlateauJeuModule } from '../plateau-jeu/plateau-jeu.module';
+import { EcrainJeuComponent } from './ecrain-jeu.component';
 
 
 @NgModule({
-
+  declarations: [
+    EcrainJeuComponent
+  ],
   imports: [
     CommonModule,
-    EcrainJeuRoutingModule
+    EcrainJeuRoutingModule,
+    PlateauJeuModule
   ],
 
 })
--- front/src/app/base/Jeu/plateau-jeu/plateau-jeu.component.ts
+++ front/src/app/base/Jeu/plateau-jeu/plateau-jeu.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-plateau-jeu',
   templateUrl: './plateau-jeu.component.html',
   styleUrls: ['./plateau-jeu.component.scss']
 })
-export class PlateauJeuComponent {
+export class PlateauJeuComponent implements OnInit {
+
+  largeur = 7;
+  hauteur = 4;
+
+  button : string[][] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    for (let i = 0; i<this.hauteur+1; i++) {
+      this.button[i] = [];
+    }
+    for (let i = 0; i<this.hauteur; i++) {
+      for (let j = 0; j<this.largeur; j++) {
+        this.button[i][j] = "largeur : " + j + ", hauteur : " + i;
+      }
+    }
+  }
 
 }
