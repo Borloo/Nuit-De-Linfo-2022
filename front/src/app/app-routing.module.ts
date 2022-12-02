42236fdc443688e4e03c565d133f38abb19473fa
IcePingus <pingus32@hotmail.fr>
2022-12-02 01:36:18 +0100

    Page about us

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./base/home-page/home-page.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'about-us',
+    loadChildren: () => import('./base/about-us/about-us.module').then(m => m.AboutUsModule)
   }
 ];
 
